a1616f3b9d6c4f9c44b44aa8d59ae6ccb9fc12fd
Flores-AM <agustinmatias62@gmail.com>
2023-04-13 11:26:48 -0300

    agregadas las comillas del form

--- views/sahumerios/formulario.php
+++ views/sahumerios/formulario.php
@@ -2,22 +2,22 @@
         <legend>Informacion General</legend>
 
         <div>
-          <input type="text" name="sahumerio[nombre]" id="nombre" value="<?php echo s($sahumerio->nombre); ?>" required>
+          <input type="text" name="sahumerio['nombre']" id="nombre" value="<?php echo s($sahumerio->nombre); ?>" required>
           <label for="nombre"><span>Nombre</span></label>
         </div>
 
         <div>
-          <input type="text" name="sahumerio[marca]" id="marca" value="<?php echo s($sahumerio->marca); ?>" required>
+          <input type="text" name="sahumerio['marca']" id="marca" value="<?php echo s($sahumerio->marca); ?>" required>
           <label for="marca"><span>Marca</span></label>
         </div>
 
         <div>
-          <input type="number" name="sahumerio[precio]" id="precio" value="<?php echo s($sahumerio->precio); ?>" required>
+          <input type="number" name="sahumerio['precio']" id="precio" value="<?php echo s($sahumerio->precio); ?>" required>
           <label for="precio"><span>Precio</span></label>
         </div>
 
         <div>
-          <input type="file" name="sahumerio[imagen]" id="imagen" accept="image/jpeg, image/png">
+          <input type="file" name="sahumerio['imagen']" id="imagen" accept="image/jpeg, image/png">
           <label for="imagen"><span>Imagen</span></label>
         </div>
 
@@ -26,7 +26,7 @@
         <?php endif; ?> 
 
         <div>
-          <textarea name="sahumerio[descripcion]" id="descripcion" required><?php echo s($sahumerio->descripcion); ?></textarea>
+          <textarea name="sahumerio['descripcion']" id="descripcion" required><?php echo s($sahumerio->descripcion); ?></textarea>
           <label for="descripcion"><span>Descripcion</span></label>
         </div>
       </fieldset>
@@ -35,7 +35,7 @@
         <legend>Stock Disponible</legend>
 
         <div>
-          <input type="number" name="sahumerio[stock]" id="stock" value="<?php echo s($sahumerio->stock); ?>" min="1" max="20" required>
+          <input type="number" name="sahumerio['stock']" id="stock" value="<?php echo s($sahumerio->stock); ?>" min="1" max="20" required>
           <label for="stock"><span>Stock</span></label>
         </div>
 

6468faf6eab85177500c44be1e03cf3d8553127d
Flores-AM <agustinmatias62@gmail.com>
2023-04-12 21:51:54 -0300

    probando

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,13 @@
 <?php
 
 function conectarDB() : mysqli {
-  $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
+  $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
+  $db->set_charset("utf8");
 
   if(!$db) {
-    echo "No se pudo conectar";
+    echo "Error: No se pudo conectar a MySQL."; 
+    echo "errno de depuración: " . mysqli_connect_errno(); 
+    echo "error de depuración: " . mysqli_connect_error(); 
     exit;
   }
   return $db;
